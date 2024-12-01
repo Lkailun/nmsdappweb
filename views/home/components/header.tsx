095b8704d222f4d48267b827d44ab2ff8db5d83d
kailun <Lukailun820820@outlook.com>
2024-12-01 22:27:31 +0800

    up

--- views/home/components/header.tsx
+++ views/home/components/header.tsx
@@ -3,9 +3,11 @@ import css from '../styles/header.module.scss';
 import { useUser } from '@/state/user/hooks';
 import CountUp from 'react-countup';
 import moment from 'moment';
+import { useTranslation } from 'next-i18next';
 
 const Header: FC = (): ReactElement => {
     const [{ userinfo }] = useUser();
+    const { t }: any = useTranslation<any>(['common']);
 
     useEffect(() => {
         moment.locale('zh-cn');
@@ -14,7 +16,7 @@ const Header: FC = (): ReactElement => {
         <div className={css.header}>
             <div className={css.top}>
                 <div className={css.item}>
-                    <h5>我的累计场次(次)</h5>
+                    <h5>{t('common:home:my_total_game_count')}</h5>
                     <div>
                         <span>
                             <CountUp decimals={0} end={userinfo.totalgamecount} />
@@ -22,7 +24,7 @@ const Header: FC = (): ReactElement => {
                     </div>
                 </div>
                 <div className={css.item}>
-                    <h5>累计参与金额(NMS)</h5>
+                    <h5>{t('common:home:my_total_spend_nms')}</h5>
                     <div>
                         <span>
                             <CountUp decimals={5} end={userinfo.totalspendnms} />
@@ -31,7 +33,7 @@ const Header: FC = (): ReactElement => {
                     </div>
                 </div>
             </div>
-            <p>我的最近游戏: {userinfo.lastgametime === 0 ? '无记录' : moment(userinfo.lastgametime).endOf('m').fromNow()}</p>
+            <p>{t('common:home:my_recent_game')}: {userinfo.lastgametime === 0 ? t('common:home:no_record') : moment(userinfo.lastgametime).endOf('m').fromNow()}</p>
         </div>
     );
 };
